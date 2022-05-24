9262e59b81aed6928e8fbd8ef28b3b7f8b8f3768
MIROCHNYK <76389860+SergeMiro@users.noreply.github.com>
2022-05-24 17:58:01 +0200

    Node-modules deleted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
 import Dialogs from './components/dialogs/dialogs';
 import Header from './components/header/header';
 import Navbar from './components/navbar/navbar';
 import Profile from './components/profile/profile';
-import {BrowserRouter, Route} from "react-router-dom"
+import { BrowserRouter, Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
@@ -12,8 +14,8 @@ const App = () => {
 		<Header />
 		<Navbar />
 		<div className="app-wrapper-content">
-			<Route path='/profile' component={Profile}/>
-			<Route path='/dialogs' component={Dialogs} />
+			<Route path="/profile" component={Profile}/>
+			<Route path="/dialogs" component={Dialogs}/>
 		</div>
    </div>
 	</BrowserRouter>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import mcss from './navbar.module.css'
 
 
+
 const Navbar = () =>{
 	return(
 		<nav className={mcss.navbar}>
