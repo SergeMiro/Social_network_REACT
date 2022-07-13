4ca5815a3d3c63b6118307672ffef84c95f62696
MIROCHNYK <76389860+SergeMiro@users.noreply.github.com>
2022-07-13 20:02:29 +0200

    Data added in state.jsx

--- src/components/dialogs/dialogs.jsx
+++ src/components/dialogs/dialogs.jsx
@@ -2,33 +2,14 @@ import React from 'react';
 import mcss from './dialogs.module.css'
 import ItemMessage from './itemMessage/itemMessage';
 import Message from './message/message';
-
+import state from '../../redux/state';
 
 const Dialogs = (props) =>{
-
-	let dialogsData = [
-		{id: 1, name: 'Fred', surname: 'Unbelievable'},
-		{id: 2, name: 'Serge', surname: 'Miro'},
-		{id: 3, name: 'Roman', surname: 'Orcheed'},
-		{id: 4, name: 'Alina', surname: 'Miro'},
-		{id: 5, name: 'Paul', surname: 'Van Dyk'},
-		{id: 6, name: 'Aleksey', surname: 'Fedotov'},
-		{id: 7, name: 'Volodymyr', surname: 'Golosnyouk'},
-		{id: 8, name: 'Lyoudmila', surname: 'Pryimak'},
-		{id: 9, name: 'Veronika', surname: 'Miroshnyk'},
-		{id: 10, name: 'Dasha', surname: 'Melnik'}
-	]
 	let dialogsElements =
-	dialogsData.map (el=> <ItemMessage name={el.name} surname={el.surname} id={el.id} />); //перебираем массив данных (обьектов) пришедших с сервака
+	state.messagesPage.dialogsData.map (el=> <ItemMessage name={el.name} surname={el.surname} id={el.id} />); //перебираем массив данных (обьектов) пришедших с сервака
 	/*----------------------------------------------------------------------------*/
-
-	let messagesData = [
-		{id: 1, message: 'Hi man, how r u? What do u think about this project?'},
-		{id: 2, message: 'Abrakadabra work for this network.'},
-		{id: 3, message: 'Blalblabla gavno-kod in this Bananas Network.'}
-	]
 	let messagesElements =
-	messagesData.map(el=> <Message message={el.message}/>);
+	state.messagesPage.messagesData.map(el=> <Message message={el.message}/>);
 
 	return(
 		<div className={mcss.container}>
--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -3,16 +3,17 @@ import AvatarDescription from './avaDescriptionPosts/avaDescription';
 import NewPost from './myPosts/newPost/newPost';
 import Post from './myPosts/post/post';
 import mcss from './profile.module.css';
+import state from '../../redux/state';
 
 const Profile = (props) => {
 
-	let postsData = [
-		{id: 1, post: 'Hi guys, I have some bananas', likesCount: 23},
-		{id: 2, post: 'No way, I have only one banana', likesCount: 74},
-		{id: 3, post: 'Me too, I have many bananas. Can I sale or by the bananas?', likesCount: 18},
-		{id: 4, post: "It's not a crypto... U can't sale or buy the bananas. I think so.", likesCount: 55}
-	]	
-	let postsElements = postsData.map (el => <Post myBoardPosts={el.post} myBoardLikes={el.likesCount} />);
+	// let postsData = [
+	// 	{id: 1, post: 'Hi guys, I have some bananas', likesCount: 23},
+	// 	{id: 2, post: 'No way, I have only one banana', likesCount: 74},
+	// 	{id: 3, post: 'Me too, I have many bananas. Can I sale or by the bananas?', likesCount: 18},
+	// 	{id: 4, post: "It's not a crypto... U can't sale or buy the bananas. I think so.", likesCount: 55}
+	// ]	
+	let postsElements = state.profilePage.postsData.map (el => <Post myBoardPosts={el.post} myBoardLikes={el.likesCount} />);
 
 
 		return (
